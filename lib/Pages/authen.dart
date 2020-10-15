0a19561bcfa5f0eec5eeadc879011a9fbf18cb61
Chao Chaiyakam <chaochaiyakam@Chaos-MacBook-Pro.local>
2020-10-15 17:25:37 +0700

    chao

--- lib/Pages/authen.dart
+++ lib/Pages/authen.dart
@@ -6,6 +6,117 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget titleform() {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.09,
+      width: MediaQuery.of(context).size.width * 0.9,
+      // color: Colors.white,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+        color: Colors.blue,
+        //border: Border.all(color:Colors.red),
+      ),
+      child: Center(
+          child: Text(
+        "เข้าสู่ระบบ",
+        style: TextStyle(
+            fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.bold),
+      )),
+    );
+  }
+
+  Widget emailForm() {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.8,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            // margin: EdgeInsets.all(10),
+            height: 40,
+            width: 40,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/social-media (1).png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.6,
+            child: TextFormField(
+              decoration: InputDecoration(hintText: 'อีเมล'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget passwordForm() {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.8,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            // margin: EdgeInsets.all(10),
+            height: 40,
+            width: 40,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/key.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.6,
+            child: TextFormField(
+              decoration: InputDecoration(hintText: 'รหัสผ่าน'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget loginButtom() {
+    return InkWell(
+      onTap: () {
+        print("object");
+      },
+      child: Container(
+        height: 30,
+        width: MediaQuery.of(context).size.width * 0.7,
+        // color: Colors.white,ß
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[
+                  Colors.orange,
+                  Colors.orange[900],
+                ])
+            //color: Colors.blue,
+
+            //border: Border.all(color:Colors.red),
+            ),
+
+        child: Center(
+          child: Text(
+            'เข้าสู่ระบบ',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +138,15 @@ class _AuthenState extends State<Authen> {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10.0),
               color: Colors.white,
-              border: Border.all(color:Colors.red)
+              //border: Border.all(color:Colors.red),
+            ),
+            child: Column(
+              children: [
+                titleform(),
+                emailForm(),
+                passwordForm(),
+                loginButtom(),
+              ],
             ),
           ),
         ),
