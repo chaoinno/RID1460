66801333ea52f58e5d282deb8f426544e995d45d
thanawattbest <best_vicious@hotmail.com>
2020-10-22 10:59:10 +0700

    thanawattbest

--- lib/Pages/news.dart
+++ lib/Pages/news.dart
@@ -8,19 +8,41 @@ class News extends StatefulWidget {
 }
 
 class _NewsState extends State<News> {
+  final items = List<String>.generate(10000, (i) => "Item $i");
+
   int _selectedIndex = 3;
+
+
+  PageController _pageController;
+
+    @override
+  void initState()
+  {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose()
+  {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
       if (index == 0) {
+        
         MaterialPageRoute materialPageRoute =
             MaterialPageRoute(builder: (BuildContext context) => Home());
         Navigator.of(context).push(materialPageRoute);
+        _pageController.animateToPage(index, duration: null, curve: Curves.easeOut);
       } else if (index == 2) {
         MaterialPageRoute materialPageRoute =
             MaterialPageRoute(builder: (BuildContext context) => Newcase());
         Navigator.of(context).push(materialPageRoute);
+        _pageController.animateToPage(index, duration: null, curve: Curves.easeOut);
       }
       //MaterialPageRoute materialPageRoute =
       // MaterialPageRoute(builder: (BuildContext context) => Authen());
@@ -40,14 +62,29 @@ class _NewsState extends State<News> {
             fit: BoxFit.cover,
           ),
         ),
-        child: ListView(
+        child: Column(
           children: <Widget>[
-            
-            Container(
-              height: 50,
-              color: Colors.amber[600],
-              child: const Center(child: Text('Entry A')),
-            )
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) {},
+                //controller: editingController,
+                decoration: InputDecoration(
+                    fillColor: Colors.white,
+                   
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+              ),
+            ),
+            Expanded(child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text("${items[index]}"),
+                    tileColor: Colors.white,
+                  );
+                }),),
           ],
         ),
       ),
